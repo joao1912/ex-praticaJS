9e185c111de3e203f077947dfc086cdae9bb14bf
João Pedro T <joaopedrot1912@gmail.com>
2022-10-31 20:46:14 -0300

    adicionei alguns comando básicos
    melhorei o objeto {refrescos}, vou tentar adicionar algo para funcionar como um "carrinho" e criar outro objeto.

--- exercicios/modelo/script.js
+++ exercicios/modelo/script.js
@@ -1,21 +1,37 @@
 let refresco = {
-    suco: ["Suco de abacaxi", "Suco de Morango", "Suco de Uva", "Suco de Limão"],
-    preço: ["R$ 6,00", "R$ 7,00", "R$ 8,00", "R$ 9,00"],
-    Codigo: [0,1,2,3]
+    suco: ["Suco de abacaxi", "Suco de Morango", "Suco de Uva", "Suco de Limão", "Coca-Cola", "Pepsi"],
+    preço: ["R$ 6,00", "R$ 7,00", "R$ 8,00", "R$ 9,00", "R$ 10,00" , "R$10,00"],
+    Codigo: [0,1,2,3,4,5]
+}
+
+let alimentos = {
+
 }
 
 
 
+
+let codigo = document.getElementById("cod")
+let item = document.createAttribute("imput:number")
+
 function mostrar() {
-    alert(refresco.suco[0])
     let bebidas = document.getElementById("Bebidas")
     let lanches = document.getElementById("lanches")
     let resp = document.getElementById("resp")
     if (bebidas.checked) {
         resp.innerHTML ="<p>Opcões de bebidas a baixo:</p>"
-        for ( let c=0 ; c < 4; c++) {
-            resp.innerHTML = `<p>${refresco.suco[c]} | preço: ${refresco.preço[c]} | Código: ${refresco.Codigo[c]}<br/></p>`
+        for ( let c=0 ; c < 6; c++) {
+            resp.innerHTML += `<p>${refresco.suco[c]} | preço: ${refresco.preço[c]} | Código: ${refresco.Codigo[c]}<br/></p>`
         }
-        
+        let pedido = document.getElementById("txtC")
+        pedido.removeAttribute("disabled")
+
+    } else if (lanches.checked) {
+
+        resp.innerHTMl = "<p>Opcões de Lanches a baixo:</p>"
+        for (let c = 0 ; c < 4 ; c++) {
+            resp.innerHTML += ``
+        }
     }
+    
 }
